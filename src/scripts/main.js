658fec8e927aa7680d12faa0df2b97cf3c656e0d
Kevin Powell <kevin@eightshapes.com>
2020-09-02 16:00:06 -0500

    Refactor page shell to web component and create off canvas nav functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
 import "@eightshapes/esds-example-code-pair/dist/esds-example-code-pair-web-component.js";
 import "@eightshapes/esds-rendered-example/dist/esds-rendered-example-web-component.js";
 import "@eightshapes/esds-icon/dist/esds-icon-web-component.js";
+import "../site-components/PageShell.js";
--- src/site/components/card/index.md
+++ src/site/components/card/index.md
@@ -1,5 +1,5 @@
 ---
-layout: "component.njk"
+layout: "local-nav.njk"
 title: "Card"
 tabsTag: card_tabs
 deck: A preview and entry point to more detailed information.
