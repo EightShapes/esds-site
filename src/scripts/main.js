872e14ad7365f2776b0bc92dfd9b218445064931
Kevin Powell <kevin@eightshapes.com>
2020-09-30 20:56:55 -0500

    Install tabs and confirm functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,5 @@ import "@eightshapes/esds-example-code-pair/dist/esds-example-code-pair-web-comp
 import "@eightshapes/esds-rendered-example/dist/esds-rendered-example-web-component.js";
 import "@eightshapes/esds-icon/dist/esds-icon-web-component.js";
 import "@eightshapes/esds-card/dist/esds-card-web-component.js";
+import "@eightshapes/esds-tabs/dist/esds-tabs-web-component.js";
 import "../site-components/page-shell/PageShell.js";
