e5e77e0f2244cefbca9cece4476c0daeb096ec2d
Kevin Powell <kevin@eightshapes.com>
2020-10-06 16:28:54 -0500

    install new tabs with tab changed hook

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import "@eightshapes/esds-example-code-pair/dist/esds-example-code-pair-web-component.js";
+import "@eightshapes/esds-page-navigation/dist/esds-page-navigation-web-component.js";
 import "@eightshapes/esds-rendered-example/dist/esds-rendered-example-web-component.js";
 import "@eightshapes/esds-icon/dist/esds-icon-web-component.js";
 import "@eightshapes/esds-card/dist/esds-card-web-component.js";
